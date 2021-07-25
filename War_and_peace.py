6ef0c041ce5b45a3179680e3db7390ab9a7a8745
Denis <deniro094@gmail.com>
2021-07-25 20:05:57 +0300

    Get the list of words, added cycle and condition for word with length < 5 symbols

--- War_and_peace.py
+++ War_and_peace.py
@@ -1,6 +1,10 @@
 f = str(open(r'war_and_peace.txt', 'r', encoding='utf-8').readlines())
 for symbol in [',', '.', '!', ':', '"']:
     f = f.replace(symbol, '')
-f.split()
+f = f.split()
+for i in f:
+    if len(i) < 5:
+        continue
+
 
 
