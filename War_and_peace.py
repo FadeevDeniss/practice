8064afa7f9ae599ad1189e9c6f88a23729a7e6d5
Denis <deniro094@gmail.com>
2021-07-28 21:43:52 +0300

    Added condition and variable to calculate max entries. Seems to be a problem, bc program evaluating too long

--- War_and_peace.py
+++ War_and_peace.py
@@ -1,10 +1,18 @@
+def position(x):
+    x = x[1]
+    return x
+
+
 f = str(open(r'war_and_peace.txt', 'r', encoding='utf-8').readlines())
-for symbol in [',', '.', '!', ':', '"']:
+for symbol in [',', '.', '!', ':', '"', 'n', '(', ')', '?']:
     f = f.replace(symbol, '')
-f = f.split()
-for i in f:
+list_of_numbers = []
+for i in f.split():
     if len(i) < 5:
         continue
-
-
-
+    if (i, f.count(i)) in list_of_numbers:
+        continue
+    list_of_numbers.append((i, f.count(i)))
+for j in list_of_numbers:
+    count = max(list_of_numbers, key=position(j))
+    print(count)
