1ac37ccd18495695a9102ccd58aecd17e58e7e67
Denis <deniro094@gmail.com>
2021-08-01 00:06:29 +0300

    At first we open a txt file in the 'read' mode as class for text streams, and created dict for words and number of appearances in text

---
+++ War_and_peace2.py
@@ -0,0 +1,5 @@
+# Here a wrote a program, that calculate top 10 most popular words in roman "War and peace",
+# by the Russian author Leo Tolstoy:
+
+with open('war_and_peace.txt', 'r+t', encoding='utf-8') as f:
+    dict_of_nums = {}
